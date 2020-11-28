58e788d13642ebc7af27b036f55eeb1999c38b50
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2020-11-28 19:57:57 +0900

    Remove cache

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 櫻井将太郎 on 2020/11/28.
+//  Copyright © 2020 shoutarou.sakurai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
